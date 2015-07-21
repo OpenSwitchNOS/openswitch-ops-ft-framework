e5338c10f7d5a03e4ca769325446022cec8e4fd2
Vince Mendoza <vincent.mendoza@hp.com>
2015-07-21 11:38:21 -0700

    Made the default workstation to be auto-ubunut-12-04 for RTL
    Change-Id: I33869990640cc9c8f837ec52e4e09363af835bd9
    Signed-off-by: Vince Mendoza <vincent.mendoza@hp.com>

--- lib/topology/LogicalTopologyCreate.py
+++ lib/topology/LogicalTopologyCreate.py
@@ -28,31 +28,55 @@ def LogicalTopologyCreate(**kwargs):
     
     # create the links
     if "topoLinks" in  topoDict :
-     topoLinks = str(topoDict['topoLinks'])
-     for curLink in str.split(topoLinks,','):
-       (link, dev1, dev2) = str.split(curLink, ':')
-       linkTag = ET.SubElement(headers.LOGICAL_TOPOLOGY, 'link', attrib={'name': link, 'device1': dev1, 'device2': dev2, 'rate': "any"})
-    
+        topoLinks = str(topoDict['topoLinks'])
+        for curLink in str.split(topoLinks,','):
+            (link, dev1, dev2) = str.split(curLink, ':')
+            linkTag = ET.SubElement(headers.LOGICAL_TOPOLOGY, 'link', attrib={'name': link, 'device1': dev1, 'device2': dev2, 'rate': "any"})
+            
+    # Create TopoDevices
     topoDevices = str(topoDict['topoDevices'])
     for curDev in str.split(topoDevices):
         # Search for device in target
         curDevTarget = "false"
         for curTarget in str.split(targets):
-           if curTarget == curDev:
-              curDevTarget = "true"
-              break
+            if curTarget == curDev:
+                curDevTarget = "true"
+                break
         
         deviceTag = ET.SubElement(headers.LOGICAL_TOPOLOGY, 'device', attrib={'name': curDev, 'target': curDevTarget, 'group': "NULL"})
     
     topoFilters = str(topoDict['topoFilters'])
     for curFilter in str.split(topoFilters, ','):
+        #print curFilter
         (cDev, cAttr, cVal) = str.split(curFilter, ':')
         # Search for the tag in logical topology
-        xpath = "./device[@name='"+cDev+"']"
+        xpath = ".//device[@name='"+cDev+"']"
         deviceTag = common.XmlGetElementsByTag(headers.LOGICAL_TOPOLOGY, xpath)
         if deviceTag != None:
-           attributeTag = ET.SubElement(deviceTag, 'attribute', attrib={'name': cAttr, 'value': cVal})
+            attributeTag = ET.SubElement(deviceTag, 'attribute', attrib={'name': cAttr, 'value': cVal})
         
+    # Need to inspect ETREE to see if profile is specific.  If not, lets assume auto-ubuntu-12-04 for workstations
+    xpath = ".//device/attribute[@value='workstation']/.."
+    wrkstonDevsTag = common.XmlGetElementsByTag(headers.LOGICAL_TOPOLOGY, xpath, allElements=True)
+    for curTag in wrkstonDevsTag:
+        #print curTag
+        attribute_list = curTag.iter('attribute')
+        #print "attrList "
+        #print attribute_list
+        found_profile = 0
+        for curAttr in attribute_list:
+            #print "curAttr"
+            attrName = curAttr.get('name')
+            #print attrName
+            if attrName == "system-profile":
+                found_profile = 1
+                common.LogOutput('debug', "Found system-profile attribute stated for device - not assuming auto-ubuntu-12-04")
+        if found_profile == 0:
+            # Need to add subelements 
+            common.LogOutput('debug', "No system-profile attribute found, defaulting to auto-ubuntu-12-04")
+            deviceTag = ET.SubElement(curTag, 'attribute', attrib={'name': "system-profile", 'value': "auto-ubuntu-12-04"})
+       
+       
     
     dumpString = ET.tostring(headers.LOGICAL_TOPOLOGY)
     
