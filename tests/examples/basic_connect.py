ae99f01c346f044bea056c69938f5e67e60bd2f6
Vince Mendoza <vincent.mendoza@hp.com>
2015-07-27 08:47:14 -0700

    simple cleanup commit
    Change-Id: Ibf4a6569451d5bbd1cb9b9976e1a2e9048646894
    Signed-off-by: Vince Mendoza <vincent.mendoza@hp.com>

--- tests/examples/basic_connect.py
+++ tests/examples/basic_connect.py
@@ -1,63 +1,63 @@
 common.LogOutput("info", "Sample test case code.")
-
+# This is not using the template - don't use as example test case
 # Grab the name of the switch from the eTree
 switchElement = common.XmlGetElementsByTag(headers.TOPOLOGY, ".//device/system[vendor='Edgecore']/name", allElements=True)
 numSwitches = len(switchElement)
 for switchE in switchElement:
-   switchName = switchE.text
-   # Connect to the device
-   common.LogOutput('info', "########################################")
-   common.LogOutput('info', "Connecting to switch " + switchName)
-   common.LogOutput('info', "########################################")
-   devConn = switch.Connect(switchName)
-   if devConn is None:
-      common.LogOutput('error', "Failed to connect to switch " + switchName)
-      continue
+    switchName = switchE.text
+    # Connect to the device
+    common.LogOutput('info', "########################################")
+    common.LogOutput('info', "Connecting to switch " + switchName)
+    common.LogOutput('info', "########################################")
+    devConn = switch.Connect(switchName)
+    if devConn is None:
+        common.LogOutput('error', "Failed to connect to switch " + switchName)
+        continue
 
-   # Rebooting switch
-   common.LogOutput('info', "Rebooting switch " + switchName)
-   retStruct = switch.Reboot(connection=devConn)
-   retCode = retStruct.get('returnCode')
-   if retCode != 0:
-      common.LogOutput('error', "Failed to reboot switch " + switchName)
-   else:
-      common.LogOutput('info', "Successfully rebooted switch " + switchName)
-   # Do vlan create
-   retStruct = switch.OVS.OvsBridgeConfig(connection=devConn, bridge="br0", action='config', ports=[1, 2, 3])
+    # Rebooting switch
+    common.LogOutput('info', "Rebooting switch " + switchName)
+    retStruct = switch.Reboot(connection=devConn)
+    retCode = retStruct.get('returnCode')
+    if retCode != 0:
+        common.LogOutput('error', "Failed to reboot switch " + switchName)
+    else:
+        common.LogOutput('info', "Successfully rebooted switch " + switchName)
+    # Do vlan create
+    retStruct = switch.OVS.OvsBridgeConfig(connection=devConn, bridge="br0", action='config', ports=[1, 2, 3])
 
-   vlanList = [1]
-   retStruct = switch.OVS.OvsVlanConfig(connection=devConn, bridge="br0", vlans=vlanList)
+    vlanList = [1]
+    retStruct = switch.OVS.OvsVlanConfig(connection=devConn, bridge="br0", vlans=vlanList)
 
 
-   # OvsShow
-   mystruct = switch.OVS.OvsShow(connection=devConn)
-   retCode = common.ReturnJSONGetCode(json=mystruct)
-   print mystruct
-   uuid = common.ReturnJSONGetData(json=mystruct, dataElement="Open_vSwitch_UUID")
-   common.LogOutput('info', "Open VSwitch UUID:" + str(uuid))
-   fanInfo = common.ReturnJSONGetData(json=mystruct, dataElement='Fans')
-   common.LogOutput('info', "Fan Information")
-   fans = fanInfo.keys()
-   for curFan in fans:
-      curDict = fanInfo[curFan]
-      status = curDict['status']
-      rpm = curDict['rpm']
-      speed = curDict['speed']
-      common.LogOutput('info', "\tCurrent Fan " + curFan + " status:" + str(status) + " speed:" + str(speed) + " rpm:" + str(rpm))
+    # OvsShow
+    mystruct = switch.OVS.OvsShow(connection=devConn)
+    retCode = common.ReturnJSONGetCode(json=mystruct)
+    print mystruct
+    uuid = common.ReturnJSONGetData(json=mystruct, dataElement="Open_vSwitch_UUID")
+    common.LogOutput('info', "Open VSwitch UUID:" + str(uuid))
+    fanInfo = common.ReturnJSONGetData(json=mystruct, dataElement='Fans')
+    common.LogOutput('info', "Fan Information")
+    fans = fanInfo.keys()
+    for curFan in fans:
+        curDict = fanInfo[curFan]
+        status = curDict['status']
+        rpm = curDict['rpm']
+        speed = curDict['speed']
+        common.LogOutput('info', "\tCurrent Fan " + curFan + " status:" + str(status) + " speed:" + str(speed) + " rpm:" + str(rpm))
 
-   # Temp sensor INformation
-   tempInfo = common.ReturnJSONGetData(json=mystruct, dataElement='Temp_Sensors')
-   tempKeys = tempInfo.keys()
-   common.LogOutput('info', "Temperature Info")
-   for curTemp in tempKeys:
-      curDict = tempInfo[curTemp]
-      temperature = curDict['temperature']
-      max = curDict['max']
-      min= curDict['min']
-      common.LogOutput('info', "\tCurrent Temp Sensor " + curTemp + " temperature:" + str(temperature) + " max:" + str(max) + " min:" + str(min))
+    # Temp sensor INformation
+    tempInfo = common.ReturnJSONGetData(json=mystruct, dataElement='Temp_Sensors')
+    tempKeys = tempInfo.keys()
+    common.LogOutput('info', "Temperature Info")
+    for curTemp in tempKeys:
+        curDict = tempInfo[curTemp]
+        temperature = curDict['temperature']
+        max = curDict['max']
+        min = curDict['min']
+        common.LogOutput('info', "\tCurrent Temp Sensor " + curTemp + " temperature:" + str(temperature) + " max:" + str(max) + " min:" + str(min))
 
-   otherConfig = common.ReturnJSONGetData(json=mystruct, dataElement='other_config')
-   common.LogOutput('info', "Other Configuration")
-   keys_List = otherConfig.keys()
-   for curKey in keys_List:
-      common.LogOutput('info', "\t" + str(curKey) + ": " +  otherConfig[curKey])
+    otherConfig = common.ReturnJSONGetData(json=mystruct, dataElement='other_config')
+    common.LogOutput('info', "Other Configuration")
+    keys_List = otherConfig.keys()
+    for curKey in keys_List:
+        common.LogOutput('info', "\t" + str(curKey) + ": " + otherConfig[curKey])
