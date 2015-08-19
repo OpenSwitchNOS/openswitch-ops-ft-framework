6fcb54372fd3bea298e5373cb062a2734f30d1b8
Vince Mendoza <vincent.mendoza@hp.com>
2015-08-19 15:00:32 -0700

    Cleaned up host library folder
    Change-Id: I4b34ab2340dfb5f6811720e1d56e8ca111a51e31
    Signed-off-by: Vince Mendoza <vincent.mendoza@hp.com>

--- lib/host/__init__.py
+++ lib/host/__init__.py
@@ -1,7 +1,3 @@
-#from Connect import *
-#from DetectConnection import *
-#from DeviceInteract import *
-#from ErrorCheck import *
 from NetworkConfig import *
 from Network6Config import *
 from DevicePing import *
