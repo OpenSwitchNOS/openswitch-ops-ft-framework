03614a146b386724c11fea737228fddb49b9f131
Jose Hernandez <jphernandez@hp.com>
2015-09-10 09:55:34 -0700

    Added libraries to generate iperf traffic from VHosts. Thanks to Diego Hurtado for coding them.
    Change-Id: I754387be0797ab3c9b1a7376957aa93f421667e9
    Signed-off-by: Jose Hernandez <jphernandez@hp.com>

---
+++ lib/Traffic_Gen/ClientStop.py
@@ -0,0 +1,70 @@
+#########################################################################################
+# Name:        switch.CLI.ClientStop
+#
+# Namespace:   switch.CLI
+#
+# Author:      Jose Pablo Hernandez 
+#
+# Purpose:     Library function to display settings configured on 1 or several LAGs
+#
+# Params:         deviceObj - Workstation identifier
+#
+# Returns:          returnCode :- status of command
+#                   data: - Dictionary:
+#                               'Client IP': Server IP address
+#                               'Client port': Client port
+#                               'Server IP': Server IP address
+#                               'Server port': Server port
+#                   buffer: - Raw output
+##PROC-###################################################################################
+
+import lib
+import pexpect
+import time
+import re
+
+def ClientStop(** kwargs):
+    #Params
+    deviceObj = kwargs.get('deviceObj', None)
+    port = kwargs.get('port', 5001)
+    
+    #If device is not passed, we need error message
+    if deviceObj is None:
+        lib.LogOutput('error', "Need to pass device to configure")
+        returnJson = lib.returnStruct(returnCode=1)
+        return returnJson
+    
+    deviceObj.expectHndl.expect(['\$', pexpect.TIMEOUT], timeout=1)
+
+    ips_and_ports = re.search('local (.*) port (\d+) connected with (.*) port (\d+)', deviceObj.expectHndl.before)
+
+    traffic_data = re.findall('sec  ([.\d]+ .*?)  ([.\d]+ .+)\r', deviceObj.expectHndl.before)
+
+    # If client fails result is None and returnList == []
+
+    server_ip = None
+    server_port = None
+    client_ip = None
+    client_port = None
+
+    if ips_and_ports is not None:
+        server_ip = ips_and_ports.group(1)
+        server_port = ips_and_ports.group(2)
+        client_ip = ips_and_ports.group(3)
+        client_port = ips_and_ports.group(4)
+
+    data_dict = {}
+
+    data_dict['Server IP'] = server_ip
+    data_dict['Server port'] = server_port
+    data_dict['Client IP'] = client_ip
+    data_dict['Client port'] = client_port
+    data_dict['Traffic data'] = traffic_data
+
+    command = r'\003'
+    deviceObj.expectHndl.send(command)
+    deviceObj.expectHndl.expect('#')
+
+    #Compile information to return
+    returnCls = lib.returnStruct(returnCode=0, buffer=deviceObj.expectHndl.before, data=data_dict)
+    return returnCls
