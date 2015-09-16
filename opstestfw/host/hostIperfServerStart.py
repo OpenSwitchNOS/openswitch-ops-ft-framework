154ce7718bea02615334f8843f8a5e7f9aa76e51
Jose Hernandez <jphernandez@hp.com>
2015-09-16 13:27:01 -0700

    Fixed iperf libraries command generation statements & installation check.
    Change-Id: Ie34dac00e7622735e8225a1a6ab64ec540bd4d92
    Signed-off-by: Jose Hernandez <jphernandez@hp.com>

--- opstestfw/host/hostIperfServerStart.py
+++ opstestfw/host/hostIperfServerStart.py
@@ -25,6 +25,8 @@ def hostIperfServerStart(** kwargs):
     port = kwargs.get('port', 5001)
     protocol = kwargs.get('protocol', 'TCP')
     interval = kwargs.get('interval', 1)
+    #Variables
+    bufferString = ''
     
     #If device is not passed, we need error message
     if deviceObj is None:
@@ -32,10 +34,54 @@ def hostIperfServerStart(** kwargs):
         returnJson = opstestfw.returnStruct(returnCode=1)
         return returnJson
     
+    #Verify if iperf is installed on host assuming it is Ubuntu and then install it
+    command='iperf'
+    opstestfw.LogOutput('debug', "Verifying if iperf is installed on device " + deviceObj.device)
+    deviceObj.expectHndl.sendline(command)
+    index = deviceObj.expectHndl.expect(['Usage','command not found'])
+    bufferString += str(deviceObj.expectHndl.before)
+    if index == 0:
+        #In case iperf is installed
+        index = deviceObj.expectHndl.expect(['# ',pexpect.TIMEOUT],timeout=5)
+        bufferString += str(deviceObj.expectHndl.before)
+        if index == 1:
+            opstestfw.LogOutput('error', "Error while verifying status of iperf on device " + deviceObj.device)
+            return opstestfw.returnStruct(returnCode=1, buffer=bufferString)
+    else:
+        #In case iperf is not installed
+        index = deviceObj.expectHndl.expect(['# ',pexpect.TIMEOUT],timeout=5)
+        bufferString += str(deviceObj.expectHndl.before)
+        if index == 1:
+            opstestfw.LogOutput('error', "Error while verifying status of iperf on device " + deviceObj.device)
+            return opstestfw.returnStruct(returnCode=1, buffer=bufferString)
+        opstestfw.LogOutput('debug', "Installing iperf")
+        command = 'apt-get install'
+        index = deviceObj.expectHndl.expect(['# ',pexpect.TIMEOUT],timeout=30)
+        bufferString += str(deviceObj.expectHndl.before)
+        if index == 1:
+            opstestfw.LogOutput('error', "Error while installing iperf on device " + deviceObj.device)
+            return opstestfw.returnStruct(returnCode=1, buffer=bufferString)
+        index = deviceObj.expectHndl.expect(['Usage','command not found',pexpect.Timeout],timeout=5)
+        bufferString += str(deviceObj.expectHndl.before)
+        if index != 0:
+            opstestfw.LogOutput('error', "Could not install iperf correctly")
+            index = deviceObj.expectHndl.expect(['# ',pexpect.TIMEOUT],timeout=5)
+            bufferString += str(deviceObj.expectHndl.before)
+            if index != 0:
+                opstestfw.LogOutput('error', "Unknown error on device")
+                return opstestfw.returnStruct(returnCode=1, buffer=bufferString)
+        else:
+            index = deviceObj.expectHndl.expect(['# ',pexpect.TIMEOUT],timeout=5)
+            bufferString += str(deviceObj.expectHndl.before)
+            if index != 0:
+                opstestfw.LogOutput('error', "Unknown error on device")
+                return opstestfw.returnStruct(returnCode=1, buffer=bufferString)
+        opstestfw.LogOutput('debug', "Successfully installed iperf on device")
+    
     command = 'iperf -s -p ' + str(port)
-    command = ' -i ' + str(interval)
+    command += ' -i ' + str(interval)
     if protocol == 'UDP':
-        command = command + ' -u'
+        command += ' -u'
 
     deviceObj.expectHndl.sendline(command)
     
