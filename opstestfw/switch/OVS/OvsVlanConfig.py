cb0e07aee7adbdccde59e20f9cfe9fdd69cdf77e
Vince Mendoza <vincent.mendoza@hp.com>
2015-09-27 13:09:46 -0700

    Pep-8 compliance on OVS routines
    Change-Id: I21918cfef59f2c6e4c660bded475c57a28ac6c18
    Signed-off-by: Vince Mendoza <vincent.mendoza@hp.com>

--- opstestfw/switch/OVS/OvsVlanConfig.py
+++ opstestfw/switch/OVS/OvsVlanConfig.py
@@ -1,63 +1,79 @@
-###PROC+#####################################################################
-# Name:        opstestfw.switch.OVS.OvsVlan
+# (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
 #
-# Namespace:   opstestfw.switch.OVS
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-# Author:      Vince Mendoza
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Purpose:     Create a vlan via ovs-vsctl
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 #
-# Params:      connection - device connection
-#              action - config / unconfig
-#              bridge - bridge to add the vlan too
-#              vlans - list of Vlans to add to the bridge
-#
-# Returns:     JSON structure
-#              returnCode - status of command
-#
-##PROC-#####################################################################
+
 
 from opstestfw import *
-import opstestfw.switch
-import re
+
 
 def OvsVlanConfig(**kwargs):
-    connection = kwargs.get('connection')
-    action = kwargs.get('action', 'config')
-    bridge = kwargs.get('bridge', 'br0')
-    vlans = kwargs.get('vlans')
 
-    if connection is None:
-       return False
+    """
+     Create a vlan via ovs-vsctl
+
+    :param device    : Device object
+    :type  device    : object
+    :param action    : config or unconfig
+    :type  action    : string
+    :param bridge    : bridge name
+    :type bridge     : string
+    :param vlans     : list of Vlans to add to the bridge "1 10 20"
+    :type vlans      : list
+
+    :return: returnStruct Object
+    :returnType: object
+    """
+
+    device = kwargs.get('device', None)
+    action = kwargs.get('action', 'config')
+    bridge = kwargs.get('bridge', 'bridge_normal')
+    vlans = kwargs.get('vlans', None)
+
+    if device is None or vlans is None:
+        retString = returnStruct(returnCode=1)
+        return retString
 
     if action == 'config':
-       #   command = "ovs-vsctl add-vlan " + bridge + " " +
-       LogOutput("info","Configuring Vlan over OVS bridge")
-       for curVlan in vlans:
-          command = "ovs-vsctl add-vlan " + bridge + " " + str(curVlan) + " admin=up"
-          # Send command to the switch
-          devIntRetStruct = opstestfw.switch.DeviceInteract(connection=connection, command=command)
-          retCode = devIntRetStruct.get('returnCode')
-          if retCode != 0:
-             LogOutput('error', "Failed to create vlan " + str(curVlan) + " over bridge " + bridge)
-             retString = returnStruct(returnCode=1)
-             return retString
-
-             buffer = devIntRetStruct.get('buffer')
+        #   command = "ovs-vsctl add-vlan " + bridge + " " +
+        LogOutput("info", "Configuring Vlan over OVS bridge")
+        for curVlan in vlans:
+            command = "ovs-vsctl add-vlan " + bridge + " " + str(curVlan)\
+                + " admin=up"
+            # Send command to the switch
+            devIntRetStruct = device.DeviceInteract(command=command)
+            retCode = devIntRetStruct.get('returnCode')
+            buffer = devIntRetStruct.get('buffer')
+            if retCode != 0:
+                LogOutput('error', "Failed to create vlan " + str(curVlan)
+                          + " over bridge " + bridge)
+                retString = returnStruct(returnCode=1, buffer=buffer)
+                return retString
     else:
-       # We are in unconfig
-       for curVlan in vlans:
-          LogOutput("info","Unconfiguring Vlan over OVS bridge")
-          command = "ovs-vsctl del-vlan " + bridge + " " + str(curVlan)
-          # Send command to the switch
-          devIntRetStruct = opstestfw.switch.DeviceInteract(connection=connection, command=command)
-          retCode = devIntRetStruct.get('returnCode')
-          if retCode != 0:
-             LogOutput('error', "Failed to delete vlan " + str(curVlan) + " over bridge " + bridge)
-             retString = returnStruct(returnCode=1)
-             return retString
+        # We are in unconfig
+        for curVlan in vlans:
+            LogOutput("info", "Unconfiguring Vlan over OVS bridge")
+            command = "ovs-vsctl del-vlan " + bridge + " " + str(curVlan)
+            # Send command to the switch
+            devIntRetStruct = device.DeviceInteract(command=command)
+            retCode = devIntRetStruct.get('returnCode')
+            buffer = devIntRetStruct.get('buffer')
+            if retCode != 0:
+                LogOutput('error', "Failed to delete vlan " + str(curVlan)
+                          + " over bridge " + bridge)
+                retString = returnStruct(returnCode=1, buffer=buffer)
+                return retString
 
-             buffer = devIntRetStruct.get('buffer')
-
-    retString = returnStruct(returnCode=0)
+    retString = returnStruct(returnCode=0, buffer=buffer)
     return retString
